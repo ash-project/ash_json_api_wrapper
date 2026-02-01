c58c31271e9157b351039395bb9f31ce89a33516
Leon Qadirie <leon.qadirie@nulltree.xyz>
2026-02-01 16:00:37 +0100

    chore: SPDX contributor links

--- lib/open_api/resource_generator.ex
+++ lib/open_api/resource_generator.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2021 ash_json_api_wrapper contributors <https://github.com/ash-project/ash_json_api_wrapper/graphs.contributors>
+# SPDX-FileCopyrightText: 2021 ash_json_api_wrapper contributors <https://github.com/ash-project/ash_json_api_wrapper/graphs/contributors>
 #
 # SPDX-License-Identifier: MIT
 
